e07af62b8f2ac996472d61101c9a63a691429bf1
Loren Sklar <lorensklar@gmail.com>
2025-08-22 11:56:55 -0400

    Fix railway url for backend

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,8 +2,8 @@ export const config = {
 	verbose: import.meta.env.DEV,
 	websocket: import.meta.env.DEV 
 		? 'ws://localhost:8080/ws/python' 
-		: 'wss://pybuilderapi.railway.internal/ws/python',
+		: 'wss://pybuilderapi-production.up.railway.app/ws/python',
 	graphql: import.meta.env.DEV 
 		? 'http://localhost:8080/graphql' 
-		: 'https://pybuilderapi.railway.internal/graphql'
+		: 'https://pybuilderapi-production.up.railway.app/graphql'
 };
